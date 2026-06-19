f99eedf44515b284e59212bf1cd085ece3aa603d
DRetlaw <maildwal@gmail.com>
2026-06-19 17:17:18 +0530

    Update Factorial.java

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,1 +1,13 @@
 //Factorial implementation using Recursion
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        System.out.print(factorial(6));
+    }
+    
+    private static int factorial(int n){
+        if(n==0 || n==1)
+            return n;
+        return n * factorial(n-1);
+    }
+}
